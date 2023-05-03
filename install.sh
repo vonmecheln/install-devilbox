ccd4fc27a6166a46c95fc4eeb1ceb8f1d6a0619f
Luis Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2023-05-03 09:53:08 -0300

    Update install.sh
    Preserve config TLD_SUFFIX

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ cp env-example .env
 HOST_PORT_MYSQL=3307
 HOST_PORT_PGSQL=5433
 
-sed -i 's/^\(TLD_SUFFIX\s*=\s*\).*$/\1loc/' .env
+# sed -i 's/^\(TLD_SUFFIX\s*=\s*\).*$/\1loc/' .env
 sed -i 's#^\(TIMEZONE\s*=\s*\).*$#\1America/Sao_Paulo#' .env
 sed -i 's/^\(PHP_MODULES_ENABLE\s*=\s*\).*$/\1bcmath, ctype, fileinfo, json, mbstring, openssl, PDO, Tokenizer, XML, imagick/' .env
 sed -i 's/^\(HTTPD_DOCROOT_DIR\s*=\s*\).*$/\1public/' .env
